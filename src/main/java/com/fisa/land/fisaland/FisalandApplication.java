f2c1388cb72fc3e76b6198458637aa5f19204119
seungji2001 <lisa2009343123456@gmail.com>
2024-08-10 11:48:36 +0900

    build : 초기 설정

---
+++ src/main/java/com/fisa/land/fisaland/FisalandApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.land.fisaland;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisalandApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FisalandApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fisa/land/fisaland/FisalandApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.land.fisaland;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisalandApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
