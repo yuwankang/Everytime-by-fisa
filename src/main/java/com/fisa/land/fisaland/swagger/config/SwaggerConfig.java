51e31a49d098b3f49fc35edebb43948576738da5
unknown <wnwjdqkr@gmail.com>
2024-08-13 10:58:53 +0900

    feat: modelmapper 추가

--- src/main/java/com/fisa/land/fisaland/swagger/config/SwaggerConfig.java
+++ src/main/java/com/fisa/land/fisaland/swagger/config/SwaggerConfig.java
@@ -1,11 +1,12 @@
 package com.fisa.land.fisaland.swagger.config;
 
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.OpenAPI;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
 @OpenAPIDefinition(
         info = @Info(title = "fisaland swagger 명세서",
