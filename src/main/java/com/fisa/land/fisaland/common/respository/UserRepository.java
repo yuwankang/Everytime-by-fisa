0d3353b96909a833b5d47360a58260f08aef035f
smartcow <phw2088@gmail.com>
2024-08-13 11:03:02 +0900

    feat: User 기능 관련 기본 폴더 구조 정리

---
+++ src/main/java/com/fisa/land/fisaland/common/respository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.fisa.land.fisaland.common.respository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class UserRepository {
+
+}
---
+++ src/main/java/com/fisa/land/fisaland/common/service/UserService.java
@@ -0,0 +1,8 @@
+package com.fisa.land.fisaland.common.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+	
+}
