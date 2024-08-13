ee8de9e433fb52cb5aaa7edfc8586f5f14b0f219
unknown <wnwjdqkr@gmail.com>
2024-08-13 10:52:26 +0900

    build: modelmapper, swagger 추가

--- src/main/java/com/fisa/land/fisaland/market/entity/MarketReview.java
+++ src/main/java/com/fisa/land/fisaland/market/entity/MarketReview.java
@@ -1,5 +1,48 @@
 package com.fisa.land.fisaland.market.entity;
 
+import java.time.LocalDateTime;
+
+import com.fisa.land.fisaland.common.User;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+
+@ToString
+@Entity
 public class MarketReview {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long reviewId;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "market_id", nullable = false)
+    private Market market;
+
+    private String content;
+
+    private double rate;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/fisa/land/fisaland/market/repository/MarketReviewRepository.java
+++ src/main/java/com/fisa/land/fisaland/market/repository/MarketReviewRepository.java
@@ -1,5 +1,10 @@
 package com.fisa.land.fisaland.market.repository;
 
-public class MarketReviewRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.fisa.land.fisaland.market.entity.MarketReview;
+
+@Repository
+public interface MarketReviewRepository extends JpaRepository<MarketReview, Long> {
 }
