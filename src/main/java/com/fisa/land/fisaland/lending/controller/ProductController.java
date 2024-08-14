b0b01560540edf93cc9b50e93cae7b72e084af37
jeonguk0201 <jeonguk11@naver.com>
2024-08-14 10:58:14 +0900

    feat: 내가 올른 물건 조회, 물건 삭제 기능 추가

--- src/main/java/com/fisa/land/fisaland/lending/controller/ProductController.java
+++ src/main/java/com/fisa/land/fisaland/lending/controller/ProductController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,36 +21,38 @@ import com.fisa.land.fisaland.lending.service.ProductService;
 import jakarta.servlet.http.HttpSession;
 
 
+
 @RestController
 @RequestMapping("village")
 public class ProductController {
 
     @Autowired
     private ProductService productService;
-
+    
+    //물건 등록
     @PostMapping("addProduct")
     public Long addProduct(@RequestBody ProductDTO.CreateProduct createProduct) {
         Long userId = productService.saveProduct(createProduct);
 		return userId;
 
     }
-    
+    //등록된 모든 물건 검색
     @GetMapping("products")
     public List<ProductDTO.getProduct> getProducts() {
     	return productService.getProductList();
         
     }
-    
+    // 물건 id 로 검색
     @GetMapping("/{productId}")
     public ProductDTO.getProduct getProduct(@PathVariable("productId") Long productId) {
     	return productService.getProduct(productId);
     }
-    
+    //대여 신청
     @PatchMapping("/products/{productId}/rent")
     public void rentProduct(@PathVariable("productId") Long productId) {
         productService.updateProductStatus(productId, Product.Status.RENTED);
     }
-    
+    //반납 신청
     @PatchMapping("products/{productId}/return")
     public ResponseEntity<String> returnProduct(@PathVariable("productId") Long productId, HttpSession session) {
         // 세션에서 유저 ID를 가져옵니다.
@@ -63,5 +66,24 @@ public class ProductController {
         }
     }
     
+    //유저ID로 내가 올린 물건들 검색
+    @GetMapping("myProducts")
+    public List<ProductDTO.getMyProduct> myProducts(HttpSession session) {
+    	Long userId = (Long) session.getAttribute("userId");
+    	
+    	return productService.getProductsByUserId(userId);
+    }
+    
+    @DeleteMapping("products/{productId}/delete")
+    public ResponseEntity<String> cancelProduct(@PathVariable("productId") Long productId, HttpSession session) {
+        Long userId = (Long) session.getAttribute("userId");
+        boolean isCancelled = productService.deleteProduct(productId, userId);
+        
+        if (isCancelled) {
+            return ResponseEntity.ok("Product cancelled successfully.");
+        } else {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).body("You are not authorized to cancel this product.");
+        }
+    }
     
 }
