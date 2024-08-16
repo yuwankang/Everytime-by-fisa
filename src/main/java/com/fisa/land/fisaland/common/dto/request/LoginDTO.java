3751847ae98c42c3eb7c83efc25cd5f59048c77c
yuwankang <kangyuwan@naver.com>
2024-08-16 11:58:16 +0900

    fix: 사용자 pw 로그 추출되는 버그 해결

--- src/main/java/com/fisa/land/fisaland/common/dto/request/LoginDTO.java
+++ src/main/java/com/fisa/land/fisaland/common/dto/request/LoginDTO.java
@@ -6,15 +6,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-@ToString
 public class LoginDTO {
-	
+
     @NotNull(message = "이메일 주소는 필수입니다.")
     @Schema(description = "사용자의 이메일 주소", example = "user@example.com")
     private String email;
@@ -22,4 +20,10 @@ public class LoginDTO {
     @NotNull(message = "비밀번호는 필수입니다.")
     @Schema(description = "사용자의 비밀번호", example = "password123")
     private String pw;
+    
+    //로그인 로그 추출시 pw 가져오는 버그 방지
+    @Override
+    public String toString() {
+        return "LoginDTO(email=" + email + ")";
+    }
 }
